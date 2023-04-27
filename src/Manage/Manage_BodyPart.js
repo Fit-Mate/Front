691e347c89777c17f51874c637ad1200bc92eb92
cms <daumtto53@gmail.com>
2023-04-27 20:04:22 +0900

    Manage_bodypart modifying

--- src/Manage/Manage_BodyPart.js
+++ src/Manage/Manage_BodyPart.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 
-const allBPartURI = "admin/bodyParts/list?cookie={}";
+const allbPartURI = "admin/bodyParts/list?cookie={}";
 
 const getPageFirstIndex = (currentPage) => {
 	return ((currentPage - 1) * 10);
@@ -11,18 +11,6 @@ const getPageLastIndex = (currentPage) => {
 	return (currentPage * 10 - 1);
 }
 
-//Batch가 아닌 Batch의 object 하나만 받음.
-const makeTableHead = (obj) => {
-	const keys = object.keys();
-	const tablehead = keys.map((key) => {
-		<th>{key}</th>
-	});
-	return tablehead;
-};
-
-const makeTableRow = (obj) => {
-
-}
 
 const Manage_BodyPart = () => {
 	/**
@@ -36,18 +24,59 @@ const Manage_BodyPart = () => {
 	 *
 	 * naviageButtonClicked===1:next, === -1:prev ===0:notClicked
 	 */
-	const [bPartArray, setBPartArray] = React.useState([]);
+	const [bPartArray, setbPartArray] = React.useState([]);
 	const [currentPage, setCurrentPage] = React.useState(1);
-	cosnt [navigateButtonClicked, setNaviageButtonClicked] = React.useState(0);
 
 	/**
 	 * Functions
-	 * getAllBPart : 운동정보 데이터 모두 fetch
+	 * getAllbPart : 운동정보 데이터 모두 fetch
 	 */
+	const getAllbPart = async () => {
+		const movies = await axios.get(allbPartURI);
+		setbPartArray(movies);
+	}
 
-	const getAllBPart = async () => {
-		const movies = await axios.get(allBPartURI);
-		setBPartArray(movies);
+	/**
+	 * Rendering Function
+	 */
+	//Batch가 아닌 Batch의 object 하나만 받음.
+	const makeTableHead = (bodyParts) => {
+		const keys = object.keys();
+		const tablehead = keys.map((key) => {
+			<th>{key}</th>
+		});
+		return (
+			<tr>
+				{tablehead}
+			</tr>
+		);
+	};
+
+	const makeTableBodyElements = (bPartBatch) => {
+		const columns = bPartBatch.map((bPart) => {
+			return (
+				<tr>
+					<td>{bPart.englishName}</td>
+					<td>{bPart.koreanName}</td>
+					<td>
+						<button id={bPart.id} onClick={handleModifybPart}></button>
+					</td>
+				</tr>
+			);
+		})
+	};
+
+	/**
+	 * Handler
+	 */
+	const handleModifybPart = (event) => {
+		console.log(event.target.id);
+	}
+
+	const handleNavigatePage = (event) => {
+		if (event.target.id === "prev"){
+			if (currentPage === )
+		}
 	}
 
 
@@ -58,7 +87,7 @@ const Manage_BodyPart = () => {
 	 */
 
 	React.useEffect(() => {
-		getAllBPart();
+		getAllbPart();
 		//
 	}, [])
 
@@ -68,22 +97,25 @@ const Manage_BodyPart = () => {
 
 	//bPartBatch를 currentPage에 대해서 반환.
 	React.useEffect(() => {
-		const getBPartBatch = (bPartArray, currentPageNum) => {
-			if (bPartArray.length < getPageLastIndex(currentPageNum)){
+		const getbPartBatch = (bPartArray, currentPageNum) => {
+			if (bPartArray.length < getPageLastIndex(currentPageNum)) {
 				return bPartArray.slice(getPageFirstIndex(currentPageNum), -1);
 			}
-			else{
-				return bPartArray.slice(getPageFirstIndex, )
+			else {
+				return bPartArray.slice(getPageFirstIndex,)
 			}
 		}
-		bPartBatch = getBPartBatch(bPartArray, currentPage);
+		bPartBatch = getbPartBatch(bPartArray, currentPage);
 	}, [currentPage]);
 
 	return (
 		//table render
 		//navigateButton
 		<React.fragemnt>
-
+			<makeTableHead />
+			<makeTableBodyElements />
+			<button id="prevPage" onClick={handleNavigatePage}>Prev</button>
+			<button id="nextPage" onClick={handleNavigatePage}>Next</button>
 		</React.fragemnt>
 	);
 };

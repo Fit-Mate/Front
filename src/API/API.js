8de630956a0feac8d6978a1633cac573fe1c710b
cms <daumtto53@gmail.com>
2023-05-08 03:02:56 +0900

    REVISED: API -> withCredentials:true for Cookie

--- src/API/API.js
+++ src/API/API.js
@@ -5,12 +5,14 @@ import axios from 'axios';
  * Manage_Supplement API
  */
 export const supplementCategoryAPI = axios.create({
-	baseURL: '/admin/supplement'
+	baseURL: '/admin/supplement',
+	withCredentials: true
 });
 
 
 export const supplementAPI = axios.create({
-	baseURL: '/admin/supplements'
+	baseURL: '/admin/supplements',
+	withCredentials: true
 });
 
 export const supplementPostAPI = axios.create({
@@ -19,11 +21,13 @@ export const supplementPostAPI = axios.create({
 	headers: {
 		"Content-Type": "multipart/form-data",
 	},
+	withCredentials: true
 });
 
 export const supplementPutAPI = axios.create({
 	method: 'put',
 	baseURL: '/admin/supplements',
+	withCredentials: true
 });
 
 /**
@@ -31,7 +35,8 @@ export const supplementPutAPI = axios.create({
  */
 
 export const workoutAPI = axios.create({
-	baseURL: '/admin/workouts'
+	baseURL: '/admin/workouts',
+	withCredentials: true
 });
 
 export const workoutPostAPI = axios.create({
@@ -40,11 +45,13 @@ export const workoutPostAPI = axios.create({
 	headers: {
 		"Content-Type": "multipart/form-data",
 	},
+	withCredentials: true
 });
 
 export const workoutPutAPI = axios.create({
 	method: 'put',
 	baseURL: '/admin/workouts',
+	withCredentials: true
 });
 
 
@@ -52,7 +59,8 @@ export const workoutPutAPI = axios.create({
  * Manage_Machine API
  */
 export const machineAPI = axios.create({
-	baseURL: '/admin/machines'
+	baseURL: '/admin/machines',
+	withCredentials: true
 });
 
 export const machinePostAPI = axios.create({
@@ -60,11 +68,13 @@ export const machinePostAPI = axios.create({
 	baseURL: '/admin/machines',
 	headers: {
 	},
+	withCredentials: true
 });
 
 export const machinePutAPI = axios.create({
 	method: 'put',
 	baseURL: '/admin/machines',
+	withCredentials: true
 });
 
 
@@ -73,16 +83,19 @@ export const machinePutAPI = axios.create({
  */
 export const bodyPartAPI = axios.create({
 	baseURL: '/admin/bodyParts',
+	withCredentials: true
 });
 
 export const bodyPartPostAPI = axios.create({
 	method: 'post',
 	baseURL: '/admin/bodyParts',
+	withCredentials: true
 });
 
 export const bodyPartPutAPI = axios.create({
 	method: 'put',
 	baseURL: '/admin/bodyParts',
+	withCredentials: true
 });
 
 
@@ -90,11 +103,13 @@ export const bodyPartPutAPI = axios.create({
 export const loginPostAPI = axios.create({
 	method: 'post',
 	baseURL: '/login',
+	withCredentials: true
 });
 
 export const logoutPuuAPI= axios.create({
 	method: 'put',
 	baseURL: '/logout',
+	withCredentials: true
 });
 
 
@@ -103,32 +118,38 @@ export const logoutPuuAPI= axios.create({
  * User API
  */
 export const userAPI = axios.create({
-	baseURL: '/user'
+	baseURL: '/user',
+	withCredentials: true
 });
 
 export const userPostAPI = axios.create({
 	method: 'post',
 	baseURL: '/user',
+	withCredentials: true
 });
 
 export const userPutAPI = axios.create({
 	method: 'put',
 	baseURL: '/user',
+	withCredentials: true
 });
 
 export const userDeleteAPI = axios.create({
 	method: 'post',
 	baseURL: '/user/delete',
+	withCredentials: true
 });
 
 export const userPasswordAPI = axios.create({
 	method: 'post',
 	baseURL: '/user/password',
+	withCredentials: true
 });
 
 export const userIdVerifyAPI = axios.create({
 	method: 'post',
 	baseURL: '/user/verify/id',
+	withCredentials: true
 });
 
 
@@ -136,17 +157,20 @@ export const userIdVerifyAPI = axios.create({
  * User API
  */
 export const bodyDataAPI = axios.create({
-	baseURL: '/bodyData'
+	baseURL: '/bodyData',
+	withCredentials: true
 });
 
 export const bodyDataPostAPI= axios.create({
 	method: 'post',
 	baseURL: '/bodyData',
+	withCredentials: true
 });
 
 export const bodyDataDeleteAPI = axios.create({
 	method: 'post',
 	baseURL: '/bodyData/delete',
+	withCredentials: true
 });
 
 
@@ -156,11 +180,13 @@ export const bodyDataDeleteAPI = axios.create({
 export const nonAdminBodyPartAPI = axios.create({
 	method: 'get',
 	baseURL: '/bodyParts',
+	withCredentials: true
 });
 
 export const nonAdminMachineAPI = axios.create({
 	method: 'post',
 	baseURL: '/machines',
+	withCredentials: true
 });
 
 /**
@@ -169,4 +195,5 @@ export const nonAdminMachineAPI = axios.create({
 export const recommendWorkoutPostAPI = axios.create({
 	method: 'post',
 	baseURL: '/recommendation',
+	withCredentials: true
 });

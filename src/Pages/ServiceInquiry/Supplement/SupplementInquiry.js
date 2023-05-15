77064a8b05b5b8c5441a0982093528c7c6c5681f
cms <daumtto53@gmail.com>
2023-05-15 22:32:06 +0900

    Supplement Image

--- src/Pages/ServiceInquiry/Supplement/SupplementInquiry.js
+++ src/Pages/ServiceInquiry/Supplement/SupplementInquiry.js
@@ -1,10 +1,13 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { userSupplementImageAPI } from "../../../API/API";
 import Button from "../../../UI/Button";
+import { Buffer } from "buffer";
 /**
  * @param {*} props : supplement, handleModalClose
  */
 const SupplementInquiry = (props) => {
+
+	const [supplementImage, setSupplementImage] = useState(null);
 
 	const supplement = { ...props.supplement };
 	const entries = Object.entries(supplement).filter(([key, value]) => (value !== 0 && value !== null));
@@ -20,8 +23,27 @@ const SupplementInquiry = (props) => {
 		)
 	});
 
+	/**function */
+	const getSupplementInfo = async () => {
+		const imageRes = await userSupplementImageAPI.get(`/image/${supplement['id']}`);
+		let result = (imageRes && imageRes.data) || [];
+		let base64ImageString = Buffer.from(imageRes.data, 'binary').toString('base64');
+		let srcValue = `data:${imageRes.headers["Content-Type"]};base64,${base64ImageString}`;
+		setSupplementImage(srcValue);
+	}
+
+	/**useEffect */
+	useEffect(() => {
+		getSupplementInfo();
+	}, [])
+
+
+
+
 	return (
 		<div>
+			<p>image</p>
+			<img width="100" height="100" src={supplementImage} />
 			<ul>
 				{description}
 			</ul>
